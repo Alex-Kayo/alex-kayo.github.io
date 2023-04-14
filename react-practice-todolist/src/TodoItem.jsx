e3c6c81434a65a5579f5df07c2798b554a9a7248
Alex-Kayo <3kayo4137@gmail.com>
2023-04-14 16:09:54 +0300

    adding react practice project todolist to my github

---
+++ react-practice-todolist/src/TodoItem.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+export function TodoItem({id, title, completed, toggleTodo, deleteTodo}) {
+    return (
+        <li key={id}>
+            <label htmlFor="">
+                <input
+                    type="checkbox"
+                    checked={completed}
+                    onChange={e => toggleTodo(id, e.target.checked)}
+                />
+                {title}
+            </label>
+            <button onClick={() => deleteTodo(id)} className="btn btn-danger">Delete</button>
+        </li>
+    )
+}
